956184ff0f654286d08d2e77d0872d5febe2627f
HerlynMae <torresherlynmae@gmail.com>
2024-06-17 10:20:22 +0800

    with tab js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
+// toggle menu
+
 //we will select the header nav and toggle menu
 // const variable cannot be changed once it has been declared.
 
@@ -10,3 +12,32 @@ toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   backDrop.classList.toggle("open");
 });
+
+// tabs
+const tabs = document.querySelectorAll(".offer__img");
+const contents = document.querySelectorAll(".offer__details");
+//the purpose of this is to put an active in all images at one
+// the purpose of this is to put an active in all images at one
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    removeActiveTab(); //remove active tab is just a variable
+    tab.classList.add("active");
+    const activeContent = document.querySelector(`#${tab.id}-content`); // $ it is used as its alias and used it to know the string that needs to be imported in JS and it is shorthand for the getElementByID method
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+//remove active tab is just a variable
+function removeActiveTab() {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+}
